fea497cef5aa027468aa8d0a705cbb2e4b496c09
Ivan Malinovski <ivan@malinov.ski>
2020-12-02 20:44:36 +0100

    Update day1.py

--- day1.py
+++ day1.py
@@ -1,18 +1,15 @@
-passwords = open('advent_of_code/day 2/passwords.txt').read().split("\n")
-part1, part2 = 0, 0
-for line in passwords:
-    line = line.split()
-    numbers = line[0].split('-')
-    first_num, last_num = int(numbers[0]), int(numbers[1])
-    letter = line[1][0]
-    password = line[2]
-    letter_count = password.count(letter)
+nums = open('numbers.txt').read().split()
+def a1():
+    for x in nums:
+        for y in nums: 
+            if int(x) + int(y) == 2020:
+                return int(x)*int(y)
+print(a1())
 
-    if letter_count >= first_num and letter_count <= last_num: # part 1
-        part1 += 1
-
-    if letter == password[first_num-1] or letter == password[last_num-1]: # part 2
-        if password[first_num-1] != password[last_num-1]:
-            part2 += 1
-
-print(part1, part2)
+def a2():
+    for x in nums:
+        for y in nums:
+            for z in nums: 
+                if int(x) + int(y) + int(z) == 2020:
+                    return int(x)*int(y)*int(z)
+print(a2())
