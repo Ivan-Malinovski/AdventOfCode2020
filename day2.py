8f91d8f3abc43bea69c0f891c295b8c15360b291
Ivan Malinovski <ivan@malinov.ski>
2020-12-02 21:02:47 +0100

    Update day2.py

--- day2.py
+++ day2.py
@@ -1,18 +1,18 @@
 passwords = open('advent_of_code/day 2/passwords.txt').read().split("\n")
-part1, part2 = 0, 0
+part1, part2 = 0, 0 # counters
 for line in passwords:
-    line = line.split()
-    numbers = line[0].split('-')
-    first_num, last_num = int(numbers[0]), int(numbers[1])
-    letter = line[1][0]
-    password = line[2]
-    letter_count = password.count(letter)
+    line = line.split() # splits the line into three parts
+    numbers = line[0].split('-') # splits the numbers into separate parts
+    first_num, last_num = int(numbers[0]), int(numbers[1]) # numbers
+    letter = line[1][0] # required letter
+    pw = line[2] # password string
+    letter_count = pw.count(letter) # counts the letter in question
 
     if letter_count >= first_num and letter_count <= last_num: # part 1
         part1 += 1
 
-    if letter == password[first_num-1] or letter == password[last_num-1]: # part 2
-        if password[first_num-1] != password[last_num-1]:
+    if letter == pw[first_num-1] or letter == pw[last_num-1]: # part 2
+        if pw[first_num-1] != pw[last_num-1]:
             part2 += 1
 
 print(part1, part2)
